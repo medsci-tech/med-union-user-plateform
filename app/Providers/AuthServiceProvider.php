6cc0e7b968b90d72cbe6ee4abfdfa5a2bdb97ac9
蔡乾 <cai.q@foxmail.com>
2016-11-07 16:33:23 +0800

    fix .env.travis

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Laravel\Passport\Passport;
+
+Passport::pruneRevokedTokens();
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -27,5 +30,8 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+
+        Passport::tokensExpireIn(Carbon::now()->addDays(15));
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
     }
 }
