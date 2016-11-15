a5748b91343ad9df7c6db617c2a86c538fa4c533
zhaiyu <zhaiyu_hb@hotmail.com>
2016-11-15 14:59:44 +0800

    修复：home页面模板错位

--- resources/views/oauth/dashboard.blade.php
+++ resources/views/oauth/dashboard.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
         <div class="row">
             @include('layouts.navigator')
 
@@ -14,6 +13,5 @@
                 </div>
             </div>
         </div>
-    </div>
 
 @endsection
