1df98b0049032bfb12153413ad99be40195ef274
zhaiyu <zhaiyu_hb@hotmail.com>
2016-11-21 14:13:42 +0800

    修复：user role checkbox and change delete button is btn-danger

--- app/Http/Controllers/Business/UserController.php
+++ app/Http/Controllers/Business/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Business;
 
+use App\Core\Authorization\Role\Role;
+use App\Core\Authorization\Role\RoleUser;
 use App\User;
 use App\Http\Controllers\CommonController;
 use App\Http\Requests\Business\User\UpdateUserRequest;
@@ -40,8 +42,19 @@ class UserController extends CommonController
      */
     public function edit($id)
     {
+        $user = User::find($id);
+        $roles = $user->roles;
+        $role_ids = [];
+        if ($roles){
+            foreach ($roles as $role){
+                $role_ids[] = $role->id;
+            }
+        }
+
         return view('business.users.edit', [
-            'user' => User::find($id)
+            'user' => $user,
+            'role_ids' => $role_ids,
+            'roles' => Role::all(),
         ]);
     }
 
@@ -52,6 +65,17 @@ class UserController extends CommonController
      */
     public function update(UpdateUserRequest $request, $id)
     {
+        RoleUser::where(['user_id' => $id])->delete();
+        $role_ids = $request->input('role_id');
+        if ($role_ids){
+            foreach ($role_ids as $role_id){
+                RoleUser::create([
+                    'user_id' => $id,
+                    'role_id' => $role_id,
+                ]);
+            }
+        }
+
         User::find($id)->update($request->all());
 
         return redirect(route('users.show', ['id' => $id]))->with([
--- resources/views/business/users/edit.blade.php
+++ resources/views/business/users/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app', [
     'panel_heading' => [
-        'url' => route('enterprises.show',['id' => $user->id]),
+        'url' => route('users.show',['id' => $user->id]),
         'name' => $user->name,
     ],
 ])
@@ -9,8 +9,22 @@
     <form role="form" action="{{route('users.update',['id' => $user->id])}}" method="post" id="form-validate">
         @include('layouts.edit_form_common', ['request_id' => $user->id])
         <div class="form-group">
-            <label for="name">昵称</label>
+            <label for="name">姓名</label>
             <input id="name" name="name" value="{{$user->name}}" type="text" placeholder="" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="role">角色</label>
+            <div class="control-list checkbox">
+                @foreach($roles as $role)
+                <label class="" for="role">{{$role->label}}
+                    <input name="role_id[]"
+                           @if(in_array($role->id, $role_ids))
+                                   checked
+                                   @endif
+                           type="checkbox" value="{{$role->id}}"  required>
+                </label>
+                @endforeach
+            </div>
         </div>
         <input type="submit" class="btn btn-default">
     </form>
